8bee554af2badd95974a0d742f8ae6d7696f1664
Daniel Bound <s3719027@student.rmit.edu.au>
2020-07-29 17:49:57 +1000

    Added rough UML and ERD diagrams for backend structure

--- BackEnd/src/main/java/com/rmit/myapi/BookingApplicationAPI.java
+++ BackEnd/src/main/java/com/rmit/myapi/BookingApplicationAPI.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyapiApplication {
+public class BookingApplicationAPI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyapiApplication.class, args);
+		SpringApplication.run(BookingApplicationAPI.class, args);
 	}
 
 }
