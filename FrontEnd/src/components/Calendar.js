90dfbe77aa202860748296df75551cf0b55957ea
s3694543 <s3694543@rmit.edu.vn>
2020-08-28 13:32:08 +1000

    Finished 90% of calendar code, but no comments yet

--- FrontEnd/src/components/Calendar.js
+++ FrontEnd/src/components/Calendar.js
@@ -19,23 +19,84 @@ export class Calendar extends Component {
     currentMonth: "",
     currentDate: "",
     lastDay: "",
-    "firstDayIndex:": "",
+    firstDayIndex: "",
     prevLastDay: "",
+    lastDayIndex: "",
+    date: new Date(),
   };
-  componentDidMount() {
-    const date = new Date();
+  nextMonth = () => {
+    const date = this.state.date;
+    date.setMonth(date.getMonth() + 1);
     this.setState({
       currentMonth: this.state.months[date.getMonth()],
       currentDate: date.toDateString(),
+    });
+    this.calendar();
+  };
+  previousMonth = () => {
+    const date = this.state.date;
+    date.setMonth(date.getMonth() - 1);
+    this.setState({
+      currentMonth: this.state.months[date.getMonth()],
+      currentDate: date.toDateString(),
+    });
+    this.calendar();
+  };
+  componentDidMount() {
+    this.calendar();
+  }
+ 
+  calendar = () => {
+    const date = this.state.date;
+    date.setDate(1);
+    this.setState({
+      currentMonth: this.state.months[date.getMonth()],
+      currentDate: new Date().toDateString(),
       lastDay: new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(),
       firstDayIndex: date.getDay(),
       prevLastDay: new Date(date.getFullYear(), date.getMonth(), 0).getDate(),
+      lastDayIndex: new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+      ).getDay(),
     });
-  }
-  
+  };
+
   render() {
+    const previousMontLastDays = () => {
+      const previousDays = [];
+      for (let i = this.state.firstDayIndex; i > 0; i--) {
+        previousDays.push(
+          <td class="border px-4 py-2 bg-gray-500">
+            {this.state.prevLastDay - i + 1}
+          </td>
+        );
+      }
+      return previousDays;
+    };
+    const nextMonthDays = () => {
+      const nextMonthDays = [];
+      let nextDays = 7 - this.state.lastDayIndex - 1;
+      for (let i = 1; i <= nextDays; i++) {
+        nextMonthDays.push(<td class="border px-4 py-2 bg-gray-500">{i}</td>);
+      }
+      return nextMonthDays;
+    };
+   const createRows = (n) =>{
+     const rows =[]
+     for (let i = 1; i <= 7; i++) {
+       if(7 - previousMontLastDays().length + i + n<=31){
+        rows.push( <td class="border px-4 py-2 hover:bg-blue-500">
+        {7 - previousMontLastDays().length + i+n}
+      </td>)
+       }
+     }
+     return rows
+    };
+
     return (
-      <div className="max-w-lg container">
+      <div className="max-w-lg container border-2 border-blue-500">
         <div className="flex justify-evenly bg-blue-500">
           <svg
             class="fill-current text-white inline-block pr-4 w-12"
@@ -43,6 +104,7 @@ export class Calendar extends Component {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
+            onClick={this.previousMonth}
           >
             <path
               strokeLinecap="round"
@@ -61,6 +123,7 @@ export class Calendar extends Component {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
+            onClick={this.nextMonth}
           >
             <path
               strokeLinecap="round"
@@ -70,7 +133,7 @@ export class Calendar extends Component {
             />
           </svg>
         </div>
-        <table class="table-auto border-2 border-blue-500 ">
+        <table class="table-auto ">
           <thead>
             <tr>
               <th class="px-6 py-2">Sun</th>
@@ -79,139 +142,36 @@ export class Calendar extends Component {
               <th class="px-5 py-2">Wed</th>
               <th class="px-5 py-2">Thu</th>
               <th class="px-6 py-2">Fri</th>
-              <th class="px-6 py-2">Sat</th>          
+              <th class="px-6 py-2">Sat</th>
             </tr>
           </thead>
           <tbody>
-    <tr>
-      <td class="border px-4 py-2 bg-gray-500">26</td>
-      <td class="border px-4 py-2 bg-gray-500">27</td>
-      <td class="border px-4 py-2 bg-gray-500">28</td>
-      <td class="border px-4 py-2 bg-gray-500">29</td>
-      <td class="border px-4 py-2 bg-gray-500">30</td>
-      <td class="border px-4 py-2 bg-gray-500">31</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">1</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2 hover:bg-blue-500">2</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">3</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">4</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">5</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">6</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">7</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">8</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2 hover:bg-blue-500">9</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">10</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">11</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">12</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">13</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">14</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">15</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2 hover:bg-blue-500">16</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">17</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">18</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">19</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">20</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">21</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">22</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2 hover:bg-blue-500">23</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">24</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">25</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">26</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">27</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">28</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">29</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2 hover:bg-blue-500">30</td>
-      <td class="border px-4 py-2 hover:bg-blue-500">31</td>
-      <td class="border px-4 py-2 bg-gray-500">1</td>
-      <td class="border px-4 py-2 bg-gray-500">2</td>
-      <td class="border px-4 py-2 bg-gray-500">3</td>
-      <td class="border px-4 py-2 bg-gray-500">4</td>
-      <td class="border px-4 py-2 bg-gray-500">5</td>
-    </tr>
-    </tbody>
-        </table>
-        {/* <div className="flex-column justify-evenly flex-wrap text-center ">
-                <div className="flex justify-evenly flex-auto">
-                <div>Sun</div>
-                <div>Mon</div>
-                <div>Tue</div>
-                <div>Wed</div>
-                <div>Thu</div>
-                <div>Fri</div>
-                <div>Sat</div>
-                <div>Sun</div>
-                </div>
-                <div className="flex justify-evenly">
-                <div className="text-black text-opacity-25">26</div>
-                <div className="hover:bg-blue-500">27</div>
-                <div className="hover:bg-blue-500 ">28</div>
-                <div className="hover:bg-blue-500">29</div>
-                <div className="hover:bg-blue-500">30</div>
-                <div className="hover:bg-blue-500">{this.state.lastDay}</div>
-              </div> */}
+            <tr>
+              {previousMontLastDays()}
+              {Array.from(
+                Array(7 - previousMontLastDays().length),
+                (_, i) => i + 1
+              ).map((i) => {
 
-        {/* <div className="flex-column justify-evenly">
-                <div className="text-black text-opacity-25">27</div>
-                <div className="hover:bg-blue-500">4</div>
-                <div className="hover:bg-blue-500">11</div>
-                <div className="hover:bg-blue-500">18</div>
-                <div className="hover:bg-blue-500 active:bg-blue-700">25</div>
-                <div className="text-black text-opacity-25">1</div>
-              </div>
-              <div className="flex-column justify-evenly  w-10">
-                <div className="text-black text-opacity-25">28</div>
-                <div className="hover:bg-blue-500">5</div>
-                <div className="hover:bg-blue-500">12</div>
-                <div className="hover:bg-blue-500">19</div>
-                <div className="hover:bg-blue-500 bg-blue-500 ">26</div>
-                <div className="text-black text-opacity-25">2</div>
-              </div>
-              <div className="flex-column justify-evenly  w-10">
-                <div className="text-black text-opacity-25">29</div>
-                <div className="hover:bg-blue-500">6</div>
-                <div className="hover:bg-blue-500">13</div>
-                <div className="hover:bg-blue-500">20</div>
-                <div className="hover:bg-blue-500">27</div>
-                <div className="text-black text-opacity-25">3</div>
-              </div>
-              <div className="flex-column justify-evenly  w-10">
-                <div>Thu</div>
-                <div className="text-black text-opacity-25">30</div>
-                <div className="hover:bg-blue-500">7</div>
-                <div className="hover:bg-blue-500">14</div>
-                <div className="hover:bg-blue-500">21</div>
-                <div className="hover:bg-blue-500">28</div>
-                <div className="text-black text-opacity-25">4</div>
-              </div>
-              <div className="flex-column justify-evenly  w-10">
-                <div>Fri</div>
-                <div className="hover:bg-blue-500">1</div>
-                <div className="hover:bg-blue-500">8</div>
-                <div className="hover:bg-blue-500">15</div>
-                <div className="hover:bg-blue-500">22</div>
-                <div className="hover:bg-blue-500">29</div>
-                <div className="text-black text-opacity-25">5</div>
-              </div>
-              <div className="flex-column justify-evenly  w-10">
-                <div>Sat</div>
-                <div className="hover:bg-blue-500">2</div>
-                <div className="hover:bg-blue-500">9</div>
-                <div className="hover:bg-blue-500">16</div>
-                <div className="hover:bg-blue-500">23</div>
-                <div className="hover:bg-blue-500">30</div>
-                <div className="text-black text-opacity-25">6</div>
-              </div> */}
+                return <td class="border px-4 py-2 hover:bg-blue-500">{i}</td>;
+              })}
+            </tr>
+            <tr>
+            {createRows(0)}
+            </tr>
+            <tr>
+            {createRows(7)}
+            </tr>
+            <tr>
+            {createRows(14)}
+            </tr>
+            <tr>
+            {createRows(21)}
+            </tr>
+
+          </tbody>
+        </table>
       </div>
-      // </div>
     );
   }
 }
