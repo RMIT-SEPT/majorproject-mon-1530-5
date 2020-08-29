6974260f545b82afa927df48c05b455d75209f53
s3694543 <s3694543@rmit.edu.vn>
2020-08-29 17:09:28 +1000

    Minor updates

--- FrontEnd/src/components/Calendar.js
+++ FrontEnd/src/components/Calendar.js
@@ -29,9 +29,12 @@ export class Calendar extends Component {
   //Sets up the values for the calendar
   calendar = () => {
     const date = this.state.date;
-    let currentDate = this.state.date;
+    
+    const currentDate = new Date()
+    currentDate.setFullYear(this.state.date.getFullYear())
     // Setting the current day of the month to first day on the date object
     // This is done to find out the day of the week this day corresponds too
+
     date.setDate(1); 
     this.setState({
       currentMonth: this.state.months[date.getMonth()], // Current month on the calendar
