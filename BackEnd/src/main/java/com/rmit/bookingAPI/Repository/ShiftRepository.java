7a65dedee076a964070d955c37b637e6cca018ec
s3694543 <s3694543@rmit.edu.vn>
2020-09-21 15:03:18 +1000

    Deleted the files in backend folder

--- BackEnd/src/main/java/com/rmit/bookingAPI/Repository/ShiftRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rmit.bookingAPI.Repository;
-
-import com.rmit.bookingAPI.Model.Shift;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShiftRepository extends CrudRepository<Shift, Long> {
-}
