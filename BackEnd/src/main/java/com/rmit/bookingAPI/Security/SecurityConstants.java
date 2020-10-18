daf25f9bdeb2d181dafb15a6ce7af702e82846c9
Daniel Bound <s3719027@student.rmit.edu.au>
2020-10-18 21:51:00 +1100

    folder issue

--- BackEnd/src/main/java/com/rmit/bookingAPI/Security/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.rmit.bookingAPI.security;
-
-public class SecurityConstants {
-
-    public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-    public static final int EXPIRATION_TIME = 5 * 60 * 1000; //5 Minutes
-}
